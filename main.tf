06cb911ee0a79bb88f0a654d373b6e3f50b8a6f8
karunakar bhogyari <karunakar1008@gmail.com>
2023-07-02 21:42:06 +0530

    add initial code to set up rg

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.46.0"
+    }
+  }
+#   backend "remote" {
+#     hostname = "app.terraform.io"
+#     organization = "karunakar1008organization"
+#     workspaces {
+#       name = "ws_dev_terraformcloudbackend"
+#     }
+#   }
+}
+
+# Authenticate using azure CLI
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-nextops-vedios-demo2"
+  location = "East Us"
+}
