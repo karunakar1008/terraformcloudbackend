2e92397bcb2a6b8b2e47a8efc80f1f0c21f03e09
karunakar bhogyari <karunakar1008@gmail.com>
2023-07-02 22:26:05 +0530

    add tags

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.46.0"
     }
   }
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "karunakar1008organization"
     workspaces {
       name = "ws_dev_terraformcloudbackend"
@@ -22,4 +22,10 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "rg-nextops-vedios-demo2"
   location = "East Us"
+  tags = {
+    "environment" = "dev",
+    "createdby"   = "karunakar",
+    "location"    = "east us"
+  }
+
 }
